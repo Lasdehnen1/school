e6ea621963ce065d7318c36397ae80c65931327d
Evgenii.Pichugov <eugepic@yandex.ru>
2023-09-25 18:26:36 +0300

    4.2. Управление схемами баз данных

--- src/main/resources/templates/scripts422.sql
+++ src/main/resources/templates/scripts422.sql
@@ -8,7 +8,7 @@ create table car (
 create table car_owner(
 	id serial primary key,
 	name varchar(50) not null,
-	age serial,
+	age int,
 	has_license boolean default false,
 	car_id serial references car(id)
 )
